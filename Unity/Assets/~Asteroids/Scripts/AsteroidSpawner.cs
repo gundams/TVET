84e1b392f7094b0627bb66a36cbb7e62ac6154e6
gundams <272916786mo@gmail.com>
2018-05-08 15:57:59 +1000

    make a new games

---
+++ Unity/Assets/~Asteroids/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Asteroids
+{
+    public class AsteroidSpawner : MonoBehaviour
+    {
+        public GameObject[] asteroidPrefabs;
+        public float spawnRate = 1f;
+        public float spawnRadius = 5f;
+
+        // Use this for initialization
+        void Spawn()
+        {
+            Vector3 rand = Random.insideUnitSphere * spawnRadius;
+            rand.z = 0f;
+            Vector3 position = transform.position + rand;
+            int randIndex = Random.Range(0, asteroidPrefabs.Length);
+            GameObject randAsteroid = asteroidPrefabs[randIndex];
+            GameObject clone = Instantiate(randAsteroid);
+            clone.transform.position = position;
+        }
+        void Start()
+        {
+            InvokeRepeating("Spawn", 0, spawnRate);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
