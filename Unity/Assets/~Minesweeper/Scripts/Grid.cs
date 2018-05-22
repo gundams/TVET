2809ad64f17f33d1540b82c31d7039f1f0660706
gundams <272916786mo@gmail.com>
2018-05-22 15:56:02 +1000

    start  Minesweeper

---
+++ Unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+        Tile SpawnTile(Vector3 pos)
+        {
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos;
+            Tile currentTile = clone.GetComponent<Tile>();
+            return currentTile;
+        }
+
+    }
+}
---
+++ Unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        //Functions & Variables go here
+        public int x, y;
+        public bool isMine = false; //Is the current tile a mine?
+        public bool isRevealed = false;  //Has the tile already been revealed?
+        [Header("References")]
+        public Sprite[] emptySprites; //List of empty sprites i.e, empty, 1, 2, 3,etc...
+        public Sprite[] mineSprites; // The mine sprites
+        private SpriteRenderer rend; // Reference to sprite renderer
+
+        void Awake()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            //Checks if tile is a mine
+            if(isMine)
+            {
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
